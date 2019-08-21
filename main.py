b708cca85e21b1bd97ed48a4177e3fa1bfd934a7
Ritwik Puri <ritwikpuri5678@gmail.com>
2019-08-22 01:47:49 +0530

    basic functionality

--- main.py
+++ main.py
@@ -4,8 +4,12 @@ from bs4 import BeautifulSoup as bs
 page = requests.get(input("url of medium article/page(use quotes): "))
 soup = bs(page.text, 'html.parser')
 
-text_items = soup.find_all('p')
+heading = soup.find('h1')
+print('\n' + 'ARTICLE: ' + heading.text + '\n')
+text_items = soup.find_all('p') #gives a list of all the data/text as well as tags in the <p> tags
 
-for t in text_items:
-  text = t.contents[0]
-  print(text)
+for text in text_items:
+  if text.contents[0] == 'Written by':
+    text.decompose()
+  else:
+    print(''.join(text.strings) + '\n')
