a65922a68bf8850c2c0bc291ee635756a297f0f9
Ritwik Puri <ritwikpuri5678@gmail.com>
2019-08-24 17:32:01 +0530

    started markdowning

--- main.py
+++ main.py
@@ -1,9 +1,10 @@
-import requests
+import requests,sys
 from bs4 import BeautifulSoup as bs
 
-def scrap(soup):
+
+def scrape(x):
   heading = soup.find('h1')
-  subheading = soup.find('h2')
+  subheading = soup.find('h2') #if there's no subheading,gives author's name
   print('\n' + '-->> ' + heading.text + ': ' + subheading.text + '\n')
   text_items = soup.find_all('p') #gives a list of all the data/text as well as tags in the <p> tags
 
@@ -12,11 +13,20 @@ def scrap(soup):
       t.decompose()
       print('------end------')
     else:
-      print(''.join(t.strings) + '\n')
+      out_text = ''.join(t.strings) + '\n'
+  
+  return out_text
+
+def converter(y):
+  pass
 
 
 if __name__ == "__main__":
   page = requests.get(input("url of medium article: "), timeout=None)
+  if (page.status_code != 200):
+    print("\nInvalid....Exiting")
+    sys.exit(0)
+
   soup = bs(page.text, 'html.parser')
 
-  scrap(soup)
+  scrape(soup)
