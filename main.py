60f0fd1a436a177f40f0a1739c6cc3d23158b706
Ritwik Puri <ritwikpuri5678@gmail.com>
2019-09-03 14:59:09 +0530

    fixed a bug;improved markdown

--- main.py
+++ main.py
@@ -16,9 +16,9 @@ def scrape(x):
 
 def converter(y,head,subhead):
   with open(os.path.join(os.path.abspath('articles'),head.text + '.md'),'wb') as file:
-    file.write('# '+head.text+': '+subhead.text + '\n')
+    file.write(b'## ' + bytes(head.text, 'utf-8') + b' : ' + bytes(subhead.text, 'utf-8') + b'\n\n')
     for i in range (0,len(y)):
-      file.write('  '+y[i]+'\n\n')
+      file.write(b'  '+y[i]+b'\n\n')
   print('Done\n')
 
 if __name__ == "__main__":
